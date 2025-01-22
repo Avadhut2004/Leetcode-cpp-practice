2d221a3c61cea8a7c97629841414449a7a9a8c63
Avadhut Mane <maneavdhut26@gmail.com>
2025-01-22 17:36:52 +0530

    added recursive approach

--- 206.cpp
+++ 206.cpp
@@ -31,3 +31,21 @@ public:
         return head;
     }
 };
+
+// using recursive approach 
+// SC=O(N) TC=O(N)
+
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        if(head==NULL || head->next==NULL){
+            return head;
+        }
+        ListNode* newHead = reverseList(head->next);
+        ListNode* front = head->next;
+        front->next = head;
+        head->next = NULL;
+
+        return newHead; 
+    }
+};
