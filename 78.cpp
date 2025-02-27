bb0e8d716d4c8500f18dfc8573a41a5c3e1b4f08
Avadhut Mane <maneavdhut26@gmail.com>
2025-02-27 11:03:05 +0530

    added files

---
+++ 78.cpp
@@ -0,0 +1,19 @@
+// generating possible subsets of a power set 
+// O(2^n*n)  O(2^n);
+
+class Solution {
+    public:
+        vector<vector<int>> subsets(vector<int>& nums) {
+            vector<vector<int>>res;
+            int n = nums.size();
+    
+            for(int i=0 ;i<(1<<n);i++){
+                vector<int>curr;
+                for(int j = 0;j<n;j++){
+                    if(i&(1<<j))curr.push_back(nums[j]);
+                }
+                res.push_back(curr);
+            }
+            return res;
+        }
+    };
