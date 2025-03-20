11370a80dc84732197055c962a5034b086a42f18
Avadhut Mane <maneavdhut26@gmail.com>
2025-03-20 12:06:16 +0530

    added files

---
+++ 144.cpp
@@ -0,0 +1,18 @@
+// preorder traversal of a binary tree 
+
+class Solution {
+    public:
+        void pre(vector<int>&ans,TreeNode *root){
+            if(root == NULL)return;
+    
+            ans.push_back(root->val);
+            pre(ans,root->left);
+            pre(ans,root->right);
+        }
+        vector<int> preorderTraversal(TreeNode* root) {
+            vector<int> ans;
+            pre(ans,root);
+    
+            return ans;
+        }
+    };
