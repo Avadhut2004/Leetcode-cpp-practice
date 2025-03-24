89317130161c884dc9a88920bdcd3c839d0b5017
Avadhut Mane <maneavdhut26@gmail.com>
2025-03-24 18:15:44 +0530

    added files

---
+++ 101.cpp
@@ -0,0 +1,26 @@
+// symmetric tree 
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+    public:
+        bool symm(TreeNode* root1,TreeNode* root2){
+            if(root1==NULL || root2==NULL){
+                return(root1==root2);
+            }
+            
+            return (root1->val==root2->val)&& symm(root1->left,root2->right) && symm(root1->right,root2->left);
+        }
+        bool isSymmetric(TreeNode* root) {
+            return symm(root,root);
+        }
+    };
