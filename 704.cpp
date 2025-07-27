347b2cf5caf02262b6a55cdc22bdaca813854f00
Avadhut Mane <maneavdhut26@gmail.com>
2025-07-27 10:33:37 +0530

    changed 704 (bs)

--- 704.cpp
+++ 704.cpp
@@ -1,3 +1,6 @@
+// binary search to find a target index 
+
+// iterative approach 
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
@@ -10,5 +13,28 @@ public:
             else low = mid+1;
         }
         return -1;
+    }
+};
+
+// recursive approach 
+// TC,SC = O(logn)
+class Solution {
+public:
+
+    int bsRecursive(int low , int high , vector<int>&nums,int target){
+
+        if(low>high)return -1;
+
+        int mid = (low+high)/2;
+        if(nums[mid]==target)return mid;
+        else if(nums[mid]>target) return bsRecursive(low,mid-1,nums,target);
+        else return bsRecursive(mid+1,high,nums,target);
+    }
+
+    int search(vector<int>& nums, int target) {
+        int low=0;
+        int high = nums.size()-1;
+        return bsRecursive(low,high,nums,target);
+        
     }
 };
