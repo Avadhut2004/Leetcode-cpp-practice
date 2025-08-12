df9d81201d022a78a9d0542a18451c26e71b3b1a
Avadhut Mane <158267664+Avadhut2004@users.noreply.github.com>
2025-08-12 06:58:12 +0530

    Update 141.cpp

--- 141.cpp
+++ 141.cpp
@@ -23,3 +23,24 @@ public:
         return false;
     }
 };
+
+//  Hare and tortoise algorithm 
+
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        if(head==NULL || head->next == NULL) return false;
+
+        ListNode * slow = head;
+        ListNode* fast = head;
+
+        while(fast!= NULL && fast->next!=NULL){
+            fast=fast->next->next;
+            slow = slow->next;
+
+            if(slow==fast) return true;
+        }
+
+        return false;
+    }
+};
