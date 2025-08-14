e0dd2ee1607595af117fd0ff9ab4d183c8f250e6
Avadhut Mane <158267664+Avadhut2004@users.noreply.github.com>
2025-08-14 07:47:01 +0530

    Create 25.cpp

---
+++ 25.cpp
@@ -0,0 +1,64 @@
+// Reverse Nodes in K groups 
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* reverseLL(ListNode* head){
+        ListNode* temp = head ;
+        ListNode* prev = NULL;
+
+        while(temp!=NULL){
+            ListNode* front = temp->next ;
+            temp->next = prev;
+            prev = temp;
+            temp = front;
+        }
+
+        return prev;
+    }
+
+    ListNode* getKthNode(ListNode* temp , int k){
+        k-=1;
+        ListNode* traver = temp;
+        while(traver!= NULL && k>0){
+            traver = traver->next;
+            k--;
+        }
+        return traver;
+    }
+    ListNode* reverseKGroup(ListNode* head, int k) {
+        ListNode* temp = head ;
+        ListNode* prevLast = NULL;
+        while(temp!=NULL){
+            ListNode* kthNode = getKthNode(temp , k);
+
+            if(kthNode == NULL){
+                if(prevLast) prevLast->next = temp;
+            }
+
+            if(kthNode) ListNode* nextNode = kthNode->next;
+
+            kthNode->next = NULL;
+
+            reverseLL(temp);
+
+            if(temp==head) head=kthNode;
+            else{
+                prevLast->next = kthNode;
+            }
+            prevLast = temp;
+            temp = nextNode;
+        }
+
+        return head;
+    }
+};
