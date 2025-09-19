3551e26fdb85aa0904b0eb5ac6fee0e9fdf616d4
Avadhut Mane <158267664+Avadhut2004@users.noreply.github.com>
2025-09-19 15:11:05 +0530

    Create 155.cpp

---
+++ 155.cpp
@@ -0,0 +1,30 @@
+class MinStack {
+    private: 
+     stack<pair<int,int>> st;
+public:
+    MinStack() {
+    //    
+    }
+    
+    void push(int x) {
+        int mini;
+        if(st.empty()) mini = x;
+        else{
+            mini = min(st.top().second,x);
+        }
+        st.push({x,mini});
+    }
+    
+    void pop() {
+        st.pop();
+    }
+    
+    int top() {
+        return st.top().first;
+    }
+    
+    int getMin() {
+        return st.top().second;
+    }
+};
+
