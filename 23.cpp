7beeee6379dba5834a268f4413e5eacbb97bcd3d
Avadhut Mane <158267664+Avadhut2004@users.noreply.github.com>
2025-09-27 07:34:23 +0530

    Update 23.cpp

--- 23.cpp
+++ 23.cpp
@@ -1,3 +1,6 @@
+// this is the solution of complexity O(nlogn)
+
+
 /**
  * Definition for singly-linked list.
  * struct ListNode {
@@ -36,8 +39,49 @@ public:
         }
 
         return ans;
-    
+    }
+};
 
+
+// this is the solution with the complexity O(nlogk) (k being the no of lists )
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    struct compare{
+        bool operator()(ListNode* a, ListNode* b){
+            return a->val >b->val;
+        }
+    };
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
+        priority_queue<ListNode*,vector<ListNode*>,compare> q;
+
+        for(auto node:lists){
+            if(node) q.push(node);
+        }
+
+        ListNode dummy(0);
+        ListNode* tail = &dummy;
+
+        while(!q.empty()){
+            ListNode* curr = q.top();
+            q.pop();
+            tail->next = curr;
+            tail = tail->next;
+
+            if(curr->next) q.push(curr->next);
+        }
+
+        return dummy.next;
         
     }
 };
